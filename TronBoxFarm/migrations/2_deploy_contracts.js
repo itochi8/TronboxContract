fd24be61502deb341c16350079c1b19ac57e2888
Volkan ZULAL <43728152+volkanzulal@users.noreply.github.com>
2021-05-11 11:34:04 +0200

    fix(farms):Migration to Private Swap

--- TronBoxFarm/migrations/2_deploy_contracts.js
+++ TronBoxFarm/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var BnbStaking = artifacts.require("./BnbStaking.sol");
-var CakeToken = artifacts.require('./CakeToken.sol');
+var PrivateToken = artifacts.require('./PrivateToken.sol');
 var LotteryRewardPool = artifacts.require('./LotteryRewardPool.sol');
 var MasterChef = artifacts.require('./MasterChef.sol');
 var SousChef = artifacts.require('./SousChef.sol');
@@ -14,14 +14,14 @@ module.exports = function(deployer) {
   const endBlock = 2;
   const _wbnb = '0xaa25aa7a19f9c426e07dee59b12f944f4d9f1dd3';
   
-  deployer.deploy(CakeToken).then(function() {
-    deployer.deploy(SyrupBar,CakeToken.address).then(function() {
+  deployer.deploy(PrivateToken).then(function() {
+    deployer.deploy(SyrupBar,PrivateToken.address).then(function() {
         
-        deployer.deploy(MasterChef,CakeToken.address,SyrupBar.address,_adminAddress,rewordPerBlock,startBlock).then(function() {
-          deployer.deploy(LotteryRewardPool,MasterChef.address,CakeToken.address,_adminAddress,receiver);
+        deployer.deploy(MasterChef,PrivateToken.address,SyrupBar.address,_adminAddress,rewordPerBlock,startBlock).then(function() {
+          deployer.deploy(LotteryRewardPool,MasterChef.address,PrivateToken.address,_adminAddress,receiver);
         });
         deployer.deploy(SousChef,SyrupBar.address,rewordPerBlock,startBlock,endBlock);
-        deployer.deploy(BnbStaking,SyrupBar.address,CakeToken.address,rewordPerBlock,startBlock,endBlock,_adminAddress,_wbnb);
+        deployer.deploy(BnbStaking,SyrupBar.address,PrivateToken.address,rewordPerBlock,startBlock,endBlock,_adminAddress,_wbnb);
     });
   });
 
